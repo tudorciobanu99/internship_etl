c68100ac378677c7c4de62197f57de6bee77d2ef
Tudor <tudorciob99@outlook.com>
2025-04-15 19:06:11 +0300

    new feature in adding country

--- database_connector.py
+++ database_connector.py
@@ -11,6 +11,25 @@ class DatabaseConnector:
         rows = self.cursor.fetchall()
         return rows
 
+    def add_country(self, **params):
+        code = params.get('code')
+        name = params.get('name')
+        latitude = params.get('latitude')
+        longitude = params.get('longitude')
+        capital = params.get('capital')
+
+        query = """
+        INSERT INTO extract.country (code, name, latitude, longitude, capital)
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT (code) DO UPDATE
+        SET name = EXCLUDED.name,
+            latitude = EXCLUDED.latitude,
+            longitude = EXCLUDED.longitude,
+            capital = EXCLUDED.capital;
+        """
+        self.cursor.execute(query, (code, name, latitude, longitude, capital))
+        self.connection.commit()
+
     def fetch_countries(self):
         countries = self.fetch_rows("SELECT * FROM extract.country")
         columns = [desc[0] for desc in self.cursor.description]
