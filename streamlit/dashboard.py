17c8413cbd7ab410b9c721f0dea0df58e8171166
Tudor <tudorciob99@outlook.com>
2025-04-19 21:39:42 +0300

    documentation and small changes in the streamlit folder

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -8,10 +8,33 @@ from common.database_connector import DatabaseConnector
 import streamlit as st
 
 def connect_to_database(**db_config):
+    """
+    Initialize the DatabaseConnector object.
+
+    Args:
+        **db_config (dict): PostgreSQL database connection parameters.
+                Keys should include:
+                    dbname (str): The name of the database.
+                    user (str): The username for authentication.
+                    host (str): The database server host.
+                    password (str): The password for authentication.
+                    port (int): The port number.
+
+    Returns:
+        db (DatabaseConnector)
+    """
+
     db = DatabaseConnector(**db_config)
     return db
 
 def centered_title(title):
+    """
+    Centers a title in a given page.
+
+    Args:
+        title (str): A given title.
+    """
+
     st.markdown(
         f"""
         <h1 style="text-align: center; color: black;">{title}</h1>
@@ -20,6 +43,10 @@ def centered_title(title):
     )
 
 def override_dark_mode():
+    """
+    Overrides the dark mode for some browsers.
+    """
+
     st.markdown(
             """
             <style>
@@ -33,13 +60,21 @@ def override_dark_mode():
             unsafe_allow_html=True
     )
 
-def weather_description_format(df):
+def weather_description_format(weather_description, weather_code):
+    """
+    Aligns a weather description and weather code in a container.
+
+    Args:
+        weather_description (str): A given weather description.
+        weather_code (str): A given weather code.
+    """
+
     st.markdown(
         f"""
         <div style="text-align: left;">
             <h4>Most Frequent Weather Description</h4>
             <p style="font-size: 16px; word-wrap: break-word;">
-                {df["weather_description"][0]} ({df["weather_code"][0]})
+                {weather_description} ({weather_code})
             </p>
         </div>
         """,
@@ -47,6 +82,13 @@ def weather_description_format(df):
     )
 
 def page_config():
+    """
+    Configures a page with the proper layout and sidebars.
+
+    Returns:
+        page: The selected option.
+    """
+
     st.set_page_config(layout="wide")
     st.sidebar.title("Navigation")
     with st.sidebar.expander("Menu", expanded=True):
@@ -54,6 +96,16 @@ def page_config():
     return page
 
 def api_selector(db):
+    """
+    Creates a simple API select box, based on the weather and COVID-19 APIs.
+
+    Args:
+        db (DatabaseConnector object)
+
+    Returns:
+        selected_api (str): Selected API name.
+    """
+
     query = """
         SELECT api_name AS api FROM extract.api_info
     """
@@ -64,8 +116,19 @@ def api_selector(db):
     return selected_api
 
 def country_selector(db, key):
+    """
+    Creates a simple country select box, based on the load.dim_country table.
+
+    Args:
+        db (DatabaseConnector object)
+        key (str): A key to differentiate between multiple select boxes.
+
+    Returns:
+        selected_country (str): The name of the selected country.
+    """
+
     query = """
-        SELECT name FROM extract.country
+        SELECT country_name FROM load.country
     """
     data = db.fetch_rows(query)
     df = pd.DataFrame(data, columns=["country"])
@@ -74,18 +137,19 @@ def country_selector(db, key):
                                 options=["All countries"] + list(country_options), index=0, key=key)
     return selected_country
 
-def code_selector(db, key):
-    query = """
-        SELECT country_code FROM load.dim_country
-    """
-    data = db.fetch_rows(query)
-    df = pd.DataFrame(data, columns=["country_code"])
-    country_options = df["country_code"].unique()
-    selected_country = st.selectbox("Select Country:",
-                                options=["All countries"] + list(country_options), index=0, key=key)
-    return selected_country
-
 def date_slider(past=None):
+    """
+    Creates a simple one month date slider.
+
+    Args:
+        past (bool): If True, the slider displays the equivalent dates
+            but from 2022 (relevant for COVID-19).
+
+    Returns:
+        start_date (str): The selected start date.
+        end_date (str): THe selected end date.
+    """
+
     today = datetime.today().date()
     if past:
         today = today.replace(year=2022)
@@ -101,6 +165,13 @@ def date_slider(past=None):
     return start_date, end_date
 
 def api_and_logs(db):
+    """
+    Creates and displays the layout for the APIs and Logs page.
+
+    Args:
+        db (DatabaseConnector object)
+    """
+
     centered_title("API and Logs from the ETL Process")
     col1, _ = st.columns(2)
     with col1:
@@ -145,11 +216,18 @@ def api_and_logs(db):
         st.plotly_chart(fig)
 
 def covid_and_weather(db):
+    """
+    Creates and displays the layout for the COVID-19 And Weather data.
+
+    Args:
+        db (DatabaseConnector object)
+    """
+
     centered_title("Weather and COVID Data")
     col1, _ = st.columns(2)
     selected_country = "All countries"
     with col1:
-        selected_country = code_selector(db, key="3")
+        selected_country = country_selector(db, key="3")
 
     col11, col12 = st.columns(2)
     with col11:
@@ -167,7 +245,7 @@ def covid_and_weather(db):
                 st.metric("Last Collection Date", df["end_date"][0].strftime("%Y-%m-%d"))
                 st.metric("Total Confirmed Cases", f"{df['total_confirmed_cases'][0]:,}")
                 st.metric("Total Deaths", f"{df['total_deaths'][0]:,}")
-            weather_description_format(df)
+            weather_description_format(df["weather_description"][0], df["weather_code"][0])
         else:
             st.warning("Please select a specific country to view summary statistics!")
     col3, _ = st.columns(2)
@@ -182,7 +260,12 @@ def covid_and_weather(db):
         st.plotly_chart(fig)
 
 if __name__ == '__main__':
+    # Load the PostgreSQL database connection parameters from a .env file,
+    # located in the root directory of the project.
     load_dotenv('database_password.env')
+
+    # Constructing a db_config dictionary from the extracted parameters from
+    # the .env file.
     db_config = {
         "dbname": "etl",
         "user": "postgres",
@@ -190,10 +273,14 @@ if __name__ == '__main__':
         "password": os.environ.get("db_password"),
         "port": 5432,
     }
+
+    # Connection to the database.
     db = connect_to_database(**db_config)
 
+    # Configurating the pages.
     page = page_config()
 
+    # Selecting the pages.
     if page == "API and Logs":
         api_and_logs(db)
     elif page == "Weather and COVID data":
