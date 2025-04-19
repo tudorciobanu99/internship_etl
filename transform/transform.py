1fd17dcce5a450590ec73095a9c452a219dc4cf5
Tudor <tudorciob99@outlook.com>
2025-04-19 20:29:25 +0300

    documentation and slight formatting changes for the transform folder

--- transform/transform.py
+++ transform/transform.py
@@ -1,11 +1,33 @@
 from transform.data_transformer import DataTransformer
 from common.utils import (
-    get_json_row_count, open_file, move_file,
-    list_all_files_from_directory, get_file_details,
-    get_weather_description
+    get_json_row_count, open_file, move_file, list_all_files_from_directory,
+    get_file_details, get_weather_description
 )
 
 def process_weather_file(file, country_code, batch_date, countries, db):
+    """
+    Processes a raw file containg the extracted data from the Weather API.
+    For each given file containg weather data, the process follows the scheme:
+        1) Checks if it is a valid file containing a country code already
+            present in the extract.country table. Otherwise is moved straight
+            to the error directory associated with the weather data.
+        2) If it is validated, an initial uncomplete record in the transform_log
+            table is added.
+        3) If the data from the file can be parsed properly, it is inserted in the
+            weather_data_import table. Otherwise, the data in the file is untouched.
+        4) The file is moved to its corresponding directory depending on its status.
+        5) The initial umcomplete record in the transform_log table is updated
+            with the additional information.
+
+    Args:
+        file (str): The complete name of the file to be processed.
+        country_code (str): The country code extracted from the file name.
+        batch_date (str): The batch date extracted from the file name.
+        countries (DataFrame): A DataFrame used for validating whether the file contains
+            a valid country code from the extract.country table.
+        db (DataTransformer object)
+    """
+
     file_name = file.split("/")[-1]
     status = "error"
     p_dir_name = "data/error/weather_data/"
@@ -14,7 +36,8 @@ def process_weather_file(file, country_code, batch_date, countries, db):
         country_id = countries[countries["code"] == country_code]["id"].values[0]
 
         try:
-            db.insert_initial_transform_log(batch_date, int(country_id), "processing")
+            log_values = (batch_date, int(country_id), "processing")
+            db.insert_initial_transform_log(log_values)
 
             data = open_file(file)
             try:
@@ -29,18 +52,11 @@ def process_weather_file(file, country_code, batch_date, countries, db):
                 if not weather_description:
                     weather_description = "Unknown"
 
-                i_params = {
-                    "country_id": int(country_id),
-                    "date": date,
-                    "weather_code": str(weather_code),
-                    "weather_description": str(weather_description),
-                    "mean_temperature": float(mean_temperature),
-                    "mean_surface_pressure": float(mean_surface_pressure),
-                    "precipitation_sum": float(precipitation_sum),
-                    "relative_humidity": float(relative_humidity),
-                    "wind_speed": float(wind_speed)
-                }
-                db.insert_weather_data(**i_params)
+                insert_values = (int(country_id), date, str(weather_code),
+                                 str(weather_description),float(mean_temperature),
+                                 float(mean_surface_pressure), float(precipitation_sum),
+                                 float(relative_humidity),float(wind_speed))
+                db.insert_weather_data(insert_values)
 
                 status = "processed"
                 p_dir_name = "data/processed/weather_data/"
@@ -50,20 +66,38 @@ def process_weather_file(file, country_code, batch_date, countries, db):
             move_file(file, p_dir_name, file_name)
 
             row_count = get_json_row_count(p_dir_name, file_name)
-            u_params = {
-                "batch_date": batch_date,
-                "country_id": int(country_id),
-                "p_dir_name": p_dir_name,
-                "p_file_name": file_name,
-                "row_count": row_count,
-                "status": status
-            }
-            db.update_transform_log(**u_params)
+            update_values = (batch_date, int(country_id), p_dir_name,
+                             file_name, row_count, status)
+            db.update_transform_log(update_values)
 
         except Exception:
+            move_file(file, p_dir_name, file_name)
             db.rollback_transaction()
 
 def process_covid_file(file, country_code, batch_date, countries, db):
+    """
+    Processes a raw file containg the extracted data from the COVID-19 API.
+    For each given file containg weather data, the process follows the scheme:
+        1) Checks if it is a valid file containing a country code already
+            present in the extract.country table. Otherwise is moved straight
+            to the error directory associated with the COVID-19 data.
+        2) If it is validated, an initial uncomplete record in the transform_log
+            table is added.
+        3) If the data from the file can be parsed properly, it is inserted in the
+            weather_data_import table. Otherwise, the data in the file is untouched.
+        4) The file is moved to its corresponding directory depending on its status.
+        5) The initial umcomplete record in the transform_log table is updated
+            with the additional information.
+
+    Args:
+        file (str): The complete name of the file to be processed.
+        country_code (str): The country code extracted from the file name.
+        batch_date (str): The batch date extracted from the file name.
+        countries (DataFrame): A DataFrame used for validating whether the file contains
+            a valid country code from the extract.country table.
+        db (DataTransformer object)
+    """
+
     status = "error"
     file_name = file.split("/")[-1]
     p_dir_name = "data/error/covid_data/"
@@ -72,7 +106,8 @@ def process_covid_file(file, country_code, batch_date, countries, db):
         country_id = countries[countries["code"] == country_code]["id"].values[0]
 
         try:
-            db.insert_initial_transform_log(batch_date, int(country_id), "processing")
+            log_values = (batch_date, int(country_id), "processing")
+            db.insert_initial_transform_log(log_values)
 
             data = open_file(file)
             try:
@@ -81,14 +116,9 @@ def process_covid_file(file, country_code, batch_date, countries, db):
                 deaths = data["data"]["deaths_diff"]
                 recovered = data["data"]["recovered_diff"]
 
-                i_params = {
-                    "country_id": int(country_id),
-                    "date": date,
-                    "confirmed_cases": int(confirmed_cases),
-                    "deaths": int(deaths),
-                    "recovered": int(recovered)
-                }
-                db.insert_covid_data(**i_params)
+                insert_values = (int(country_id), date, int(confirmed_cases),
+                                 int(deaths), int(recovered))
+                db.insert_covid_data(insert_values)
 
                 status = "processed"
                 p_dir_name = "data/processed/covid_data/"
@@ -98,20 +128,30 @@ def process_covid_file(file, country_code, batch_date, countries, db):
             move_file(file, p_dir_name, file_name)
 
             row_count = get_json_row_count(p_dir_name, file_name)
-            u_params = {
-                "batch_date": batch_date,
-                "country_id": int(country_id),
-                "p_dir_name": p_dir_name,
-                "p_file_name": file_name,
-                "row_count": row_count,
-                "status": status
-            }
-            db.update_transform_log(**u_params)
+            update_values = (batch_date, int(country_id), p_dir_name,
+                             file_name, row_count, status)
+            db.update_transform_log(update_values)
 
         except Exception:
+            move_file(file, p_dir_name, file_name)
             db.rollback_transaction()
 
 def t_routine(countries, db: DataTransformer):
+    """
+    Attempts to complete the transform part of the ETL.
+    The process follows the scheme:
+        1) The files are listed from the raw directories associated with
+            each both weather and COVID-19 data.
+        2) The two tables weather_data_import and covid_data_import from
+            the transform schema are truncated.
+        3) For each kind of file, the file name is analysed, and processed
+            according to the logic specified in the process functions above.
+
+    Args:
+        countries (DataFrame): DataFrame created based on the extract.country table.
+        db (DataTransformer object)
+    """
+
     files_weather = list_all_files_from_directory("data/raw/weather_data")
     files_covid = list_all_files_from_directory("data/raw/covid_data")
 
@@ -120,10 +160,12 @@ def t_routine(countries, db: DataTransformer):
 
     for file in files_weather:
         country_code, batch_date = get_file_details(file)
-        process_weather_file(file, country_code, batch_date, countries, db)
+        if all([country_code, batch_date]):
+            process_weather_file(file, country_code, batch_date, countries, db)
 
     for file in files_covid:
         country_code, batch_date = get_file_details(file)
-        process_covid_file(file, country_code, batch_date, countries, db)
+        if all([country_code, batch_date]):
+            process_covid_file(file, country_code, batch_date, countries, db)
 
     db.close_connection()
